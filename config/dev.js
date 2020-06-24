a7c3df2b6665a2a339ea438f6aec395001106b20
Harsh Pandya <pandyaharsh43@gmail.com>
2020-06-23 23:37:43 -0700

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '559977186708-237ie6c6186ej33d2nasr9q9bu4f9is2.apps.googleusercontent.com',
+    googleClientSecret: 'XsjdIK-3VY15BBzjeFRElCwg',
+    mongoURI:'mongodb+srv://harsh:s8AnSQUfDSUZ8NTk@emaily-pevin.mongodb.net/emaily?retryWrites=true&w=majority',
+    cookieKey: 'ahdlHALDHLKahldhAIAJSLKalkfhksfmsahfash'
+};
